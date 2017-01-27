4992adab76f3628692981f06b0b580be12260c08
Arky <arkimededilorenzo@gmail.com>
2017-01-27 09:30:07 +0100

    Fatto primo layout

---
+++ app/src/main/java/kaldorenzo/it/appfindart/MainActivity.java
@@ -0,0 +1,20 @@
+package kaldorenzo.it.appfindart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView miaImmagine;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        miaImmagine = (ImageView) findViewById(R.id.img);
+        miaImmagine.setImageResource(R.drawable.img);
+
+    }
+}
